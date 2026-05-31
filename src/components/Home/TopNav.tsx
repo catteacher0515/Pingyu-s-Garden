74087ab74923df78fec5ba19f78a22dd053e3f4d
catteacher0515 <maomilaoshi0515@gmail.com>
2026-05-31 14:16:38 +0800

    fix: center brand between top and poster

--- src/components/Home/TopNav.tsx
+++ src/components/Home/TopNav.tsx
@@ -1,6 +1,6 @@
 export default function TopNav() {
   return (
-    <header className="relative z-20 px-6 pt-6 text-center lg:px-10 lg:pt-8">
+    <header className="relative z-20 flex h-28 items-center justify-center px-6 text-center lg:h-32 lg:px-10">
       <div className="mx-auto max-w-6xl text-[#f4eadf]">
         <div className="font-hand text-3xl tracking-[0.06em] text-[#ed7b62] sm:text-[2.15rem]">
           Pingyu&apos;s Garden
